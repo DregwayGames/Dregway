a55b53226341126b308fd326dea6aac643def97a
Nathan Kelly <partinarel@gmail.com>
2017-01-26 16:34:25 +1300

    Create InputObject.cs

---
+++ Objects/Input/InputObject.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dregway.Objects.Input
+{
+    abstract class InputObject
+    {
+    }
+}
